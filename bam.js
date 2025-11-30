99178b268b5e68de8c40f7f7f23f8ab55210fbdf
1amodubaruch-tech <1amodubaruch@gmail.com>
2025-11-30 21:38:31 +0100

    Add files via upload

---
+++ bam.js
@@ -0,0 +1,10 @@
+// Hamburger menu toggle
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("nav-menu");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+    hamburger.classList.toggle("open");
+  });
+});
